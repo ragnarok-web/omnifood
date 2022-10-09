142f78f7dd0e063e6a16faf6e5619ba008b8ca9f
yassine med <77589896+ragnarok-web@users.noreply.github.com>
2022-10-09 22:58:32 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// make mobile nav work
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+});
+
+const alllinks = document.querySelectorAll("a:link");
